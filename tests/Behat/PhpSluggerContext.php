5d08686d17f9458985b58d7ff136058830f0d8a9
Davor Minchorov <davorminchorov@gmail.com>
2022-09-16 11:49:03 +0200

    [AS-1214] Set up Behat and implement multiple feature tests

---
+++ tests/Behat/PhpSluggerContext.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Praetorian\Tests\PhpSlugger\Behat;
+
+use Behat\Behat\Context\Context;
+use Behat\Behat\Tester\Exception\PendingException;
+use PHPUnit\Framework\Assert;
+use Praetorian\PhpSlugger\CannotSlugifyException;
+use Praetorian\PhpSlugger\Slugger;
+
+class PhpSluggerContext implements Context
+{
+    private string $result;
+
+    /**
+     * @When /^I generate a slug from the text "([^"]*)"$/
+     * @throws CannotSlugifyException
+     */
+    public function iGenerateASlugFromTheText(string $text)
+    {
+        $this->result = (new Slugger())->slugify(text: $text);
+    }
+
+    /**
+     * @Then /^I should get the sluggified version$/
+     */
+    public function iShouldGetTheSluggifiedVersion()
+    {
+        Assert::assertEquals(expected: 'this-is-an-example-text', actual: $this->result);
+    }
+
+    /**
+     * @Given /^I generate a slug from the text "([^"]*)" with the divider "([^"]*)"$/
+     * @throws CannotSlugifyException
+     */
+    public function iGenerateASlugFromTheTextWithTheDivider(string $text, string $divider)
+    {
+        $this->result = (new Slugger())->slugify(text: $text, divider: $divider);
+    }
+
+    /**
+     * @Then /^I should get the sluggified version with the different divider$/
+     */
+    public function iShouldGetTheSluggifiedVersionWithTheDifferentDivider()
+    {
+        Assert::assertEquals(expected: 'this_is_an_example_text', actual: $this->result);
+    }
+
+    /**
+     * @Given /^I generate a slug from an empty string$/
+     */
+    public function iGenerateASlugFromAnEmptyString()
+    {
+        try {
+            $this->result = (new Slugger())->slugify(text: '');
+        } catch (CannotSlugifyException $exception) {
+            $this->result = CannotSlugifyException::class;
+        }
+    }
+
+    /**
+     * @Then /^I should expect an exception to be thrown$/
+     */
+    public function iShouldExpectAnExceptionToBeThrown()
+    {
+        Assert::assertEquals(expected: CannotSlugifyException::class, actual: $this->result);
+    }
+}
